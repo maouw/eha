1f650cd195a8146b3751f97dbaab5948fb514633
Altan Orhon <altan@ontocollage.net>
2025-03-18 03:35:05 -0700

    Changed eha optim

--- R/get_eha_optim_fun.R
+++ R/get_eha_optim_fun.R
@@ -1,32 +1,24 @@
 # This function is a wrapper for optimizer functions that allows the user to use
-# stats::optim, optimx::optimr, or optimParallel::optimParallel
-get.eha.optim.fun <- function(method = getOption("eha.optim.method", default = "BFGS")) {
-  if (startsWith(method, "optimx.")) {
-    stopifnot("Missing package 'optimx'" = requireNamespace("optimx", quietly = TRUE))
-    return(function(...) {
-      dots <- list(...)
-      if(!is.null(dots$control$fnscale)) {
-        if(as.double(dots$control$fnscale) == -1) {
-          dots$control$fnscale <- NULL
-          dots$control$maximize <- TRUE
-        } else if (as.double(dots$control$fnscale) == 1) {
-          dots$control$fnscale <- NULL
-        } else {
-          stop(method, " doesn't support a fnscale that isn't -1 or 1 (provided ", dots$control$fnscale)
-        }
+# stats::optim, ucminf::ucminf, or optimParallel::optimParallel
+get.eha.optim.fun <- function(method = getOption("eha.optim.method")) {
+  switch(method,
+    optimParallel = {
+      stopifnot("Missing package 'optimParallel'" = requireNamespace("optimParallel", quietly = TRUE))
+      function(...) {
+        optimParallel::optimParallel(...)
       }
-      dots$method <- sub("^optimx[.]", "", method)
-      do.call(optimx::optimr, dots)
-    })
-  }
-  if (method == "optimParallel") {
-    stopifnot("Missing package 'optimParallel'" = requireNamespace("optimParallel", quietly = TRUE))
-    return(function(...) {
-      optimParallel::optimParallel(...)
-    })
-  }
-  method <- match.arg(method, eval(formals(stats::optim)$method))
-  return(function(...) {
-    optim(..., method = method)
-  })
+    },
+    ucminf = {
+      stopifnot("Missing package 'ucminf'" = requireNamespace("ucminf", quietly = TRUE))
+      function(..., method = NULL) {
+        ans <- ucminf::ucminf(...)
+        if (ans$convergence == 1 || ans$convergence == 2 || ans$convergence == 4) {
+          ans$convergence <- 0
+        }
+        if ((list(...)$control$trace %||% 0) > 0) cat("ucminf message:", ans$message, "\n")
+        ans
+      }
+    },
+    \(...) stats::optim(..., method = method)
+  )
 }
